44130e3f736292176c38ce75c487754d77bb6e32
Benjamin MECHARD <benjamin.mechard-ext@sf-group.com>
2018-04-09 14:31:49 +0200

    inscription component ready

--- Front/src/app/app.module.ts
+++ Front/src/app/app.module.ts
@@ -1,16 +1,44 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  MatInputModule,
+  MatIconModule,
+  MatDatepickerModule,
+  MatNativeDateModule,
+  MatStepperModule,
+  MatButtonModule,
+  MatCardModule,
+  MatListModule
+} from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { InscriptionComponent } from './inscription/inscription.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InscriptionComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatIconModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatStepperModule,
+    MatButtonModule,
+    MatCardModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ Front/src/app/interfaces.ts
@@ -0,0 +1,8 @@
+export interface IRegister {
+    lastName: string;
+    firstName: string;
+    birthday: Date;
+    email: string;
+    password: string;
+    created: Date;
+}
