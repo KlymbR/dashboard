88091813d99cc0c29045055a21ae66cea20ea52d
Gysco <zackary.beaugelin@epitech.eu>
2018-04-26 19:35:19 -0700

    move front folder to root folder

--- old/routes/apiRouter.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const express = require('express');
-const router = express.Router();
-
-router.use('/user', require('./api/user'));
-router.use('/climbing_routes', require('./api/climbing_routes'));
-
-module.exports = router;
