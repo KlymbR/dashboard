8924b6b17957576f5bb355d9fecef05d201f2b51
Benjamin MECHARD <benjamin.mechard-ext@sf-group.com>
2018-05-15 22:57:18 +0200

    title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnDestroy, ChangeDetectorRef, OnInit } from '@angular/core';
 import { MediaMatcher } from '@angular/cdk/layout';
 import { MatDialog } from '@angular/material';
 import { DialogSigninComponent } from './dialog-signin/dialog-signin.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -12,7 +13,7 @@ export class AppComponent implements OnInit, OnDestroy {
   private _mobileQueryListener: () => void;
   public mobileQuery: MediaQueryList;
 
-  constructor(public changeDetectorRef: ChangeDetectorRef,
+  constructor(public changeDetectorRef: ChangeDetectorRef, public router: Router,
     public media: MediaMatcher, public dialogSignin: MatDialog) {
     this.mobileQuery = media.matchMedia('(max-width: 600px)');
     this._mobileQueryListener = () => changeDetectorRef.detectChanges();
@@ -25,6 +26,11 @@ export class AppComponent implements OnInit, OnDestroy {
 
   openDialogSignin(): void {
     this.dialogSignin.open(DialogSigninComponent);
+  }
+
+  title(): string {
+    const raw = this.router.url.split('/').filter(e => e.length > 0).pop();
+    return raw[0].toUpperCase() + raw.substr(1);
   }
 
   signout(): void {
