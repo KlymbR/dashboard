9b9650b7b40d98ffeb2a72ba78205516e4081c1b
Gysco <zackary.beaugelin@epitech.eu>
2019-02-07 16:44:57 +0100

    Update ading a new climbing room

--- src/app/modules/room/pages/rooms/rooms.page.ts
+++ src/app/modules/room/pages/rooms/rooms.page.ts
@@ -1,6 +1,7 @@
 import { Component, AfterViewInit } from '@angular/core';
 import { RoomService } from '@room/room.service';
 import { MatSnackBar } from '@angular/material';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-rooms',
@@ -10,13 +11,25 @@ import { MatSnackBar } from '@angular/material';
 export class RoomsComponent implements AfterViewInit {
   public loading: boolean;
   public rooms: Array<Object>;
+  public admin: boolean;
+  public roomFormGroup: FormGroup;
 
   constructor(
+    private formBuilder: FormBuilder,
     private roomService: RoomService,
     private snackBar: MatSnackBar
   ) {
     this.loading = true;
     this.rooms = [];
+    this.admin = JSON.parse(localStorage.getItem('rights'));
+  }
+
+  ngOnInit() {
+    this.roomFormGroup = this.formBuilder.group({
+      titleCtrl: ['', Validators.required],
+      latitudeCtrl: ['', Validators.required],
+      longitudeCtrl: ['', Validators.required]
+    });
   }
 
   ngAfterViewInit() {
@@ -32,4 +45,27 @@ export class RoomsComponent implements AfterViewInit {
   }
 
   link(id: string) { return '/rooms/' + id; }
+
+  public onSubmit() {
+    if (this.roomFormGroup.valid) {
+      this.loading = true;
+      const room = {
+        title: this.roomFormGroup.controls['titleCtrl'].value,
+        latitude: this.roomFormGroup.controls['latitudeCtrl'].value,
+        longitude: this.roomFormGroup.controls['longitudeCtrl'].value,
+      };
+      this.roomService.postRoom(room).subscribe((response) => {
+        this.rooms.push(response);
+        this.snackBar.open('Room created!', undefined, {
+          duration: 2000
+        });
+        this.loading = false;
+      }, (error) => {
+        this.snackBar.open(error.statusText, undefined, {
+          duration: 2000
+        });
+        this.loading = false;
+      });
+    }
+  }
 }
